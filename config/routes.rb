9ae7e35e885ebfa744d8bd0f7271dc8b7652cbbb
Prelang Builder <builder@prelang.com>
2014-07-16 23:03:42 +0000

    Uncommented the root route in 'routes.rb' and pointed it to Posts#index

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ end
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
